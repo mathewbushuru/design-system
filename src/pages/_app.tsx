82bd2523db874107fb807e4556ecc3da33f633b5
mathew bushuru <bushuruwafulamathew@gmail.com>
2023-04-05 01:48:47 -0700

    Set up chakra

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ChakraBaseProvider } from "@chakra-ui/react";
+import type { AppProps } from "next/app";
+
+import theme from "@/chakra/theme";
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraBaseProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraBaseProvider>
+  );
 }
