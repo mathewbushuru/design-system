172a7e8242cb5a124d59f8926c9bbbf5804c6b85
mathew bushuru <bushuruwafulamathew@gmail.com>
2023-04-12 01:10:17 -0700

    Showcase page overhaul

--- src/showcaseComponents/ShowcaseItem.tsx
+++ src/showcaseComponents/ShowcaseItem.tsx
@@ -1,5 +1,12 @@
 import React from "react";
-import { ListItem, ListIcon, Link } from "@chakra-ui/react";
+import {
+  ListItem,
+  ListIcon,
+  Link,
+  Image,
+  Badge,
+  Divider,
+} from "@chakra-ui/react";
 import {
   IconMoonStars,
   IconDeviceMobileCode,
@@ -10,24 +17,48 @@ interface ShowcaseItemProps {
   componentName: string;
   mobileResponsive: boolean;
   darkModeToggle: boolean;
+  ui: string;
 }
 
 const ShowcaseItem: React.FC<ShowcaseItemProps> = ({
   componentName,
   mobileResponsive,
   darkModeToggle,
+  ui,
 }) => {
   return (
-    <ListItem fontSize={20} fontWeight={600} cursor="pointer">
-      <Link href={`/showcase/${componentName}`} isExternal>
-        {componentName}{" "}
-        {darkModeToggle && <ListIcon as={IconMoonStars} color="teal" />}
-        {mobileResponsive && (
-          <ListIcon as={IconDeviceMobileCode} color="teal" />
-        )}
-        <ListIcon as={IconExternalLink} />
-      </Link>
-    </ListItem>
+    <>
+      <ListItem
+        fontSize={20}
+        fontWeight={600}
+        cursor="pointer"
+        listStyleType="none"
+        mb={10}
+        display="flex"
+        flexDir="column"
+        alignItems="center"
+      >
+        <Link href={`/showcase/${componentName}`} isExternal>
+          {componentName}{" "}
+          {darkModeToggle && <ListIcon as={IconMoonStars} color="teal" />}
+          {mobileResponsive && (
+            <ListIcon as={IconDeviceMobileCode} color="teal" />
+          )}
+          <ListIcon as={IconExternalLink} />
+        </Link>
+        <Badge variant="outline" colorScheme="teal" mt={1}>
+          {ui}
+        </Badge>
+        <Image
+          src={`/${componentName}.jpg`}
+          mt={4}
+          shadow="2xl"
+          rounded="md"
+          maxW={{ lg: "container.lg" }}
+        />
+      </ListItem>
+      <Divider/>
+    </>
   );
 };
 export default ShowcaseItem;
--- src/showcaseComponents/ShowcaseItems.tsx
+++ src/showcaseComponents/ShowcaseItems.tsx
@@ -7,65 +7,75 @@ type ShowcaseItemsProps = {};
 
 const ShowcaseItems: React.FC<ShowcaseItemsProps> = () => {
   return (
-    <UnorderedList spacing={6} mt={3}>
+    <UnorderedList spacing={6} mt={3} pb={10}>
       <ShowcaseItem
         componentName="AirbnbCard"
         mobileResponsive={true}
         darkModeToggle={true}
+        ui="ChakraUI"
       />
 
       <ShowcaseItem
         componentName="ColorSchemeToggle"
         mobileResponsive={true}
         darkModeToggle={true}
+        ui="ChakraUI"
       />
 
       <ShowcaseItem
         componentName="HeroImageReviews"
         mobileResponsive={true}
         darkModeToggle={false}
+        ui="TailwindCSS"
       />
 
       <ShowcaseItem
         componentName="HolyGrail"
         mobileResponsive={false}
         darkModeToggle={false}
+        ui="ChakraUI"
       />
 
       <ShowcaseItem
         componentName="NavbarDrawer"
         mobileResponsive={true}
         darkModeToggle={true}
+        ui="ChakraUI"
       />
 
       <ShowcaseItem
         componentName="NavbarDividedHorizontal"
         mobileResponsive={false}
         darkModeToggle={false}
+        ui="TailwindCSS"
       />
 
       <ShowcaseItem
         componentName="NavbarDividedVertical"
         mobileResponsive={false}
         darkModeToggle={true}
+        ui="ChakraUI"
       />
 
       <ShowcaseItem
         componentName="NavbarLogoBtns"
         mobileResponsive={false}
         darkModeToggle={false}
+        ui="ChakraUI"
       />
 
       <ShowcaseItem
         componentName="SimpleCart"
         mobileResponsive={true}
         darkModeToggle={true}
+        ui="ChakraUI"
       />
 
       <ShowcaseItem
         componentName="SocialMediaCard"
         mobileResponsive={true}
         darkModeToggle={true}
+        ui="ChakraUI"
       />
     </UnorderedList>
   );
