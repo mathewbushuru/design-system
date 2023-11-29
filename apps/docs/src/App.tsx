55e1fa21361954816bc9da92dee50ffb3435706f
mathew bushuru <bushuruwafulamathew@gmail.com>
2023-11-29 15:52:48 -0800

    react-router

--- apps/docs/src/App.tsx
+++ apps/docs/src/App.tsx
@@ -1,11 +1,21 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import HomePage from "@/pages/home-page";
 import ButtonPage from "@/pages/component-pages/button-page";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/components/button",
+    element: <ButtonPage />,
+  },
+]);
+
 function App() {
-  return (
-    <>
-     <ButtonPage />
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
