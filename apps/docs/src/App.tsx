930a85df32232b45731c3e0e8c775baf380a93eb
mathew bushuru <bushuruwafulamathew@gmail.com>
2023-11-29 03:09:07 -0800

    UI package config

--- apps/docs/src/App.tsx
+++ apps/docs/src/App.tsx
@@ -1,5 +1,12 @@
+import { Button } from "@mathewbushuru/ui";
+
 function App() {
-  return <div className="">Docs</div>;
+  return (
+    <div className="">
+      Docs
+      <Button />
+    </div>
+  );
 }
 
 export default App;
