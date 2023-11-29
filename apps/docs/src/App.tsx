03776b2c513903c5c4a1dba4cd047783514a7241
mathew bushuru <bushuruwafulamathew@gmail.com>
2023-11-29 04:02:36 -0800

    app:docs - mobile navbar

--- apps/docs/src/App.tsx
+++ apps/docs/src/App.tsx
@@ -1,9 +1,15 @@
+import { Menu } from "lucide-react";
 import { Button } from "@mathewbushuru/ui";
 
 function App() {
   return (
-    <div className="text-blue-900 sm:text-blue-100">
-      My design system
+    <div className="flex flex-col">
+      <nav className="flex gap-2 items-center border-b px-2 py-3">
+        <Menu className="w-5 h-5"/>
+        <p className="font-mono font-semibold tracking-widest">
+          @mathewbushuru
+        </p>
+      </nav>
       <Button />
     </div>
   );
