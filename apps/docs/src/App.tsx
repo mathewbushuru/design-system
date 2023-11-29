67287b490db137fe78190aea59921ded50ff34a2
mathew bushuru <bushuruwafulamathew@gmail.com>
2023-11-29 04:18:17 -0800

    app:docs - desktop navbar

--- apps/docs/src/App.tsx
+++ apps/docs/src/App.tsx
@@ -3,9 +3,9 @@ import { Button } from "@mathewbushuru/ui";
 
 function App() {
   return (
-    <div className="flex flex-col">
-      <nav className="flex items-center gap-2 border-b px-2 py-3 sm:gap-3 sm:px-6">
-        <Menu className="h-5 w-5" />
+    <div className="flex flex-col lg:flex-row">
+      <nav className="flex items-center gap-2 border-b px-2 py-3 sm:gap-3 sm:px-6 lg:h-screen lg:flex-col lg:items-start lg:border-b-0 lg:border-r lg:py-6">
+        <Menu className="h-5 w-5 lg:hidden" />
         <a
           className="font-mono font-semibold tracking-widest"
           target="_blank"
@@ -13,6 +13,10 @@ function App() {
         >
           @mathewbushuru
         </a>
+        <ul className="mt-12 gap-4 hidden lg:flex lg:flex-col">
+          <li className="tracking-wide cursor-pointer">Components</li>
+          <li className="tracking-wide cursor-pointer">Hooks</li>
+        </ul>
       </nav>
       <Button />
     </div>
