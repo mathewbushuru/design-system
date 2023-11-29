dab79cb7abda666e45b562a1da1589b9b1d1b920
mathew bushuru <bushuruwafulamathew@gmail.com>
2023-11-29 01:26:25 -0800

    blank slate

--- src/pages/showcase/SimpleCart.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-import SimpleCart from "@/components/forms/cart/SimpleCart";
-
-const SimpleCartPage: React.FC = () => {
-  return <SimpleCart />;
-};
-export default SimpleCartPage;
