5e4ec3913f216320ad6b81df60283fa3d92ba51f
mathew bushuru <bushuruwafulamathew@gmail.com>
2024-01-07 13:58:20 -0800

    Button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import ThemeProvider from "@/components/theme-provider";
-import ThemeToggle from "./components/theme-toggle";
+import ThemeToggle from "@/components/theme-toggle";
+
+import ButtonShowcase from "@/showcase/components/button-showcase";
 
 function App() {
   return (
@@ -7,6 +9,8 @@ function App() {
       <div className="h-screen bg-background text-foreground flex flex-col items-center p-8 gap-4">
         Design system
         <ThemeToggle />
+
+        <ButtonShowcase />
       </div>
     </ThemeProvider>
   );
---
+++ src/showcase/components/button-showcase.tsx
@@ -0,0 +1,47 @@
+import { Heart } from "lucide-react"
+
+import Button from "@/components/ui/button"
+
+export default function ButtonShowcase(){
+    return <div className="border p-6">
+        <p>Default button</p>
+        <Button>Default button</Button>
+
+        <p>Disabled button</p>
+        <Button disabled>Disabled</Button>
+
+        <h1 className="mt-4 font-semibold underline">Variants</h1>
+        <h3 className="mt-1">Default</h3>
+        <Button variant="default">Default button</Button>
+
+        <h3 className="mt-1">Destructive</h3>
+        <Button variant="destructive">Destructive button</Button>
+
+        <h3 className="mt-1">Outline</h3>
+        <Button variant="outline">Outline button</Button>
+
+        <h3 className="mt-1">Secondary</h3>
+        <Button variant="secondary">Secondary button</Button>
+
+        <h3 className="mt-1">Ghost</h3>
+        <Button variant="ghost">Ghost button</Button>
+
+        <h3 className="mt-1">Link</h3>
+        <Button variant="link">Link button</Button>
+
+        <h1 className="mt-4 font-semibold underline">Sizes</h1>
+        <h3 className="mt-1">Default</h3>
+        <Button size="default">Default button</Button>
+
+        <h3 className="mt-1">Small</h3>
+        <Button size="sm">Small button</Button>
+
+        <h3 className="mt-1">Large</h3>
+        <Button size="lg">Large button</Button>
+
+        <h3 className="mt-1">Icon</h3>
+        <Button size="icon">
+            <Heart className="h-4 w-4" />
+        </Button>
+    </div>
+}
