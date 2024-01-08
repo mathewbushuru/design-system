0e1c9b9f747cb309e87bc25fa867c84f2310f89d
mathew bushuru <bushuruwafulamathew@gmail.com>
2024-01-07 18:17:25 -0800

    layout structure

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -4,15 +4,23 @@ import ButtonShowcase from "@/showcase/components/button-showcase";
 
 const HomePage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center gap-4 bg-background p-8 text-foreground">
-      Design system
-      <div className="">
-        <ThemeToggle />
+    <div className="flex max-h-screen overflow-hidden bg-background text-foreground">
+      {/* Sidebar  */}
+      <div className="hidden min-w-40 border-r border-border lg:block">
+        Sidebar
       </div>
-      <p className="border border-border p-4 text-muted-foreground">
-        Muted text
-      </p>
-      <ButtonShowcase />
+
+      {/* Page content  */}
+      <div className="flex flex-1 flex-col items-center gap-4 overflow-y-scroll py-8">
+        Design system
+        <div className="">
+          <ThemeToggle />
+        </div>
+        <p className="border border-border p-4 text-muted-foreground">
+          Muted text
+        </p>
+        <ButtonShowcase />
+      </div>
     </div>
   );
 };
