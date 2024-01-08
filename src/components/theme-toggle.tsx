6cdf5da8bfd3f9f4c76aeb731e17012f876a04bc
mathew bushuru <bushuruwafulamathew@gmail.com>
2024-01-08 07:48:46 -0800

    mobile, tablet - navbar structure

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -18,10 +18,10 @@ export default function ThemeToggle() {
   return (
     <Button variant="ghost" onClick={handleThemeChange}>
       {theme === "light" && (
-        <Sun className="h-4 w-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+        <Sun className="h-5 w-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
       )}
       {theme === "dark" && (
-        <Moon className="h-4 w-4 rotate-90 scale-0 transition-all dark:-rotate-0 dark:scale-100" />
+        <Moon className="h-5 w-5 rotate-90 scale-0 transition-all dark:-rotate-0 dark:scale-100" />
       )}
       <span className="sr-only">Toggle theme</span>
     </Button>
--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -1,3 +1,5 @@
+import { GanttChart } from "lucide-react";
+
 import ThemeToggle from "@/components/theme-toggle";
 
 import ButtonShowcase from "@/showcase/components/button-showcase";
@@ -14,7 +16,12 @@ const HomePage = () => {
       </div>
 
       {/* Page content  */}
-      <div className="flex flex-1 flex-col items-center gap-4 overflow-y-scroll py-8">
+      <div className="flex flex-1 flex-col items-center gap-4 overflow-y-scroll pb-8 pt-20">
+        {/* Mobile Navbar  */}
+        <div className="fixed left-0 right-0 top-0 flex w-full items-center justify-between border-b px-4 py-2 lg:hidden backdrop-blur-md">
+          <GanttChart className="h-6 w-6" />
+          <ThemeToggle />
+        </div>
         Design system
         <div className="">
           <ThemeToggle />
