136fe167bf68a61ff9e8e421fd69232fd4b27802
mathew bushuru <bushuruwafulamathew@gmail.com>
2024-02-10 17:06:24 -0800

    Email company resizable panels

--- src/examples/email-company/app.tsx
+++ src/examples/email-company/app.tsx
@@ -1,7 +1,32 @@
-export default function EmailCompany(){
-    return (
-        <div className="bg-popover">
-            This is an email company
-        </div>
-    )
+import {
+  ResizablePanelGroup,
+  ResizablePanel,
+  ResizableHandle,
+} from "@/components/primitives/resizable";
+
+const defaultLayouts = [265, 440, 655];
+
+export default function EmailCompany() {
+  return (
+    <div className="bg-popover">
+      <ResizablePanelGroup
+        direction="horizontal"
+        className="max-h-[800px] min-h-48"
+      >
+        <ResizablePanel
+          defaultSize={defaultLayouts[0]}
+          minSize={15}
+          maxSize={20}
+        >
+          Sidebar
+        </ResizablePanel>
+        <ResizableHandle withHandle />
+        <ResizablePanel defaultSize={defaultLayouts[1]} minSize={30}>
+          Two
+        </ResizablePanel>
+        <ResizableHandle withHandle />
+        <ResizablePanel defaultSize={defaultLayouts[2]}>Three</ResizablePanel>
+      </ResizablePanelGroup>
+    </div>
+  );
 }
