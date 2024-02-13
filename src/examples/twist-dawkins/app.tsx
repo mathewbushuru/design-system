bb5ef162eabe84e45f269a5c1438e9a7b7415704
mathew bushuru <bushuruwafulamathew@gmail.com>
2024-02-13 05:21:56 -0800

    Twist & Dawkins LLP - News & Recongnition

--- src/examples/twist-dawkins/app.tsx
+++ src/examples/twist-dawkins/app.tsx
@@ -48,7 +48,7 @@ export default function TwistDawkins() {
 
       {/* Stats  */}
       <div className="flex flex-col gap-4 px-3 py-12 sm:flex-row sm:flex-wrap sm:justify-center sm:gap-12 sm:px-6 sm:py-20">
-        <div className="group max-w-md cursor-pointer border border-primary px-8 py-24 text-center transition-all duration-500 hover:border-none hover:shadow-2xl sm:w-[40%]">
+        <div className="group max-w-md cursor-pointer border border-primary px-8 py-24 text-center transition-all duration-500 hover:border-transparent hover:shadow-2xl sm:w-[40%]">
           <p className="font-serif text-2xl text-primary">
             185+ Years of Excellence
           </p>
@@ -57,7 +57,7 @@ export default function TwistDawkins() {
           </p>
         </div>
 
-        <div className="group group max-w-md cursor-pointer border border-primary px-8 py-24 text-center transition-all duration-500 hover:border-none hover:shadow-2xl sm:w-[40%]">
+        <div className="group group max-w-md cursor-pointer border border-primary px-8 py-24 text-center transition-all duration-500 hover:border-transparent hover:shadow-2xl sm:w-[40%]">
           <p className="font-serif text-2xl text-primary">
             Approximately 1200 Attorneys Firmwide
           </p>
@@ -66,7 +66,7 @@ export default function TwistDawkins() {
           </p>
         </div>
 
-        <div className="group max-w-md cursor-pointer border border-primary px-8 py-24 text-center transition-all duration-500 hover:border-none hover:shadow-2xl sm:w-[40%]">
+        <div className="group max-w-md cursor-pointer border border-primary px-8 py-24 text-center transition-all duration-500 hover:border-transparent hover:shadow-2xl sm:w-[40%]">
           <p className="font-serif text-2xl text-primary">
             Offices in 10 Cities Around Europe
           </p>
@@ -132,7 +132,7 @@ export default function TwistDawkins() {
         </div>
       </div>
 
-      {/* Who We Are - white  */}
+      {/* Who We Are  */}
       <div className="flex flex-col px-3 py-12 sm:px-6 sm:py-20 sm:text-foreground/80 lg:flex-row lg:py-24">
         {/* Title  */}
         <div className="lg:flex-1 lg:pl-12">
@@ -156,7 +156,72 @@ export default function TwistDawkins() {
         </div>
       </div>
 
-      {/* News and recognition - dark  */}
+      {/* News and recognition  */}
+      <div className="flex flex-col bg-secondary/20 px-3 py-12 dark:bg-primary/5 sm:px-6 sm:py-20 sm:text-foreground/80 lg:flex-row lg:py-24">
+        {/* Title  */}
+        <div className="lg:flex-1 lg:pl-12">
+          <h5 className="mb-4 font-serif text-xl tracking-wide">
+            News <br className="hidden lg:block" /> & Recognition
+          </h5>
+          <div className="mb-5 h-px max-w-12 bg-foreground dark:bg-muted-foreground sm:bg-foreground/55" />
+        </div>
+
+        {/* Content  */}
+        {/* <div className="grid grid-cols-1 gap-4 py-2 sm:grid-cols-3 sm:text-lg lg:flex-[5] lg:pr-12"> */}
+        <div className="dynamic-grid  grid gap-4 py-2 sm:text-lg lg:flex-[5] lg:pr-12">
+          <div className="group h-72 max-w-sm cursor-pointer border border-primary px-6 py-8 transition-all duration-500 hover:border-transparent hover:shadow-2xl">
+            <p className="text-primary group-hover:text-foreground/80">
+              22 June 1839 - Award
+            </p>
+            <div className="mt-6 h-[2px] max-w-6 bg-primary"></div>
+            <p className="mt-6 text-lg text-muted-foreground antialiased group-hover:text-primary">
+              Award Winners of the Decade <br /> 1839
+            </p>
+          </div>
+
+          <div className="group h-72 max-w-sm cursor-pointer border border-primary px-6 py-8 transition-all duration-500 hover:border-transparent hover:shadow-2xl">
+            <p className="text-primary group-hover:text-foreground/80">
+              15 June 1839 - Award
+            </p>
+            <div className="mt-6 h-[2px] max-w-6 bg-primary"></div>
+            <p className="mt-6 text-lg text-muted-foreground antialiased group-hover:text-primary">
+              Chambers UK Awards <br /> 1839
+            </p>
+          </div>
+
+          <div className="group h-72 max-w-sm cursor-pointer border border-primary px-6 py-8 transition-all duration-500 hover:border-transparent hover:shadow-2xl">
+            <p className="text-primary group-hover:text-foreground/80">
+              12 June 1839 - Award
+            </p>
+            <div className="mt-6 h-[2px] max-w-6 bg-primary"></div>
+            <p className="mt-6 text-lg text-muted-foreground antialiased group-hover:text-primary">
+              The Legal 500 United Kingdom 1839
+            </p>
+          </div>
+
+          <div className="group hidden h-72 max-w-sm cursor-pointer border border-primary px-6 py-8 transition-all duration-500 hover:border-transparent hover:shadow-2xl sm:block">
+            <p className="text-primary group-hover:text-foreground/80">
+              08 June 1839 - Award
+            </p>
+            <div className="mt-6 h-[2px] max-w-6 bg-primary"></div>
+            <p className="mt-6 text-lg text-muted-foreground antialiased group-hover:text-primary">
+              PILI Pro Bono Recognition Roster 1839
+            </p>
+          </div>
+
+          <div className="group hidden h-72 max-w-sm cursor-pointer border border-primary px-6 py-8 transition-all duration-500 hover:border-transparent hover:shadow-2xl sm:block">
+            <p className="text-primary group-hover:text-foreground/80">
+              02 June 1839 - Award
+            </p>
+            <div className="mt-6 h-[2px] max-w-6 bg-primary"></div>
+            <p className="mt-6 text-lg text-muted-foreground antialiased group-hover:text-primary">
+              Beacons of Justice Award
+              <br /> 1839
+            </p>
+          </div>
+        </div>
+      </div>
+
       {/* Investing in communities  */}
       {/* Charity/pro bono  */}
       {/* footer  */}
