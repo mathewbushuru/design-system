8576827847fd8443d4fb41af518072a8e9d5e50f
mathew bushuru <bushuruwafulamathew@gmail.com>
2024-02-17 20:55:21 -0800

    Twist & Dawkins - Investing in our communities

--- src/examples/twist-dawkins/app.tsx
+++ src/examples/twist-dawkins/app.tsx
@@ -20,7 +20,7 @@ export default function TwistDawkins() {
       </div>
 
       {/* Overview  */}
-      <div className="flex flex-col bg-secondary/20 px-3 py-12 dark:bg-primary/5 sm:px-6 sm:py-20 sm:text-foreground/80 lg:flex-row lg:py-24">
+      <div className="flex flex-col bg-secondary/20 px-3 py-12 dark:bg-primary/5 sm:px-6 sm:py-20 sm:text-foreground/80 lg:flex-row lg:gap-3 lg:py-24">
         {/* Title  */}
         <div className="lg:flex-1 lg:pl-12">
           <h5 className="mb-4 font-serif text-xl tracking-wide">Overview</h5>
@@ -77,7 +77,7 @@ export default function TwistDawkins() {
       </div>
 
       {/* Services  */}
-      <div className="flex flex-col bg-secondary/20 px-3 py-12 dark:bg-primary/5 sm:px-6 sm:py-20 sm:text-foreground/80 lg:flex-row lg:py-24">
+      <div className="flex flex-col bg-secondary/20 px-3 py-12 dark:bg-primary/5 sm:px-6 sm:py-20 sm:text-foreground/80 lg:flex-row lg:gap-3 lg:py-24">
         {/* Title  */}
         <div className="lg:flex-1 lg:pl-12">
           <h5 className="mb-4 font-serif text-xl tracking-wide">Services</h5>
@@ -133,7 +133,7 @@ export default function TwistDawkins() {
       </div>
 
       {/* Who We Are  */}
-      <div className="flex flex-col px-3 py-12 sm:px-6 sm:py-20 sm:text-foreground/80 lg:flex-row lg:py-24">
+      <div className="flex flex-col px-3 py-12 sm:px-6 sm:py-20 sm:text-foreground/80 lg:flex-row lg:gap-3 lg:py-24">
         {/* Title  */}
         <div className="lg:flex-1 lg:pl-12">
           <h5 className="mb-4 font-serif text-xl tracking-wide">Who We Are</h5>
@@ -157,7 +157,7 @@ export default function TwistDawkins() {
       </div>
 
       {/* News and recognition  */}
-      <div className="flex flex-col bg-secondary/20 px-3 py-12 dark:bg-primary/5 sm:px-6 sm:py-20 sm:text-foreground/80 lg:flex-row lg:py-24">
+      <div className="flex flex-col bg-secondary/20 px-3 py-12 dark:bg-primary/5 sm:px-6 sm:py-20 sm:text-foreground/80 lg:flex-row lg:gap-3 lg:py-24">
         {/* Title  */}
         <div className="lg:flex-1 lg:pl-12">
           <h5 className="mb-4 font-serif text-xl tracking-wide">
@@ -223,7 +223,58 @@ export default function TwistDawkins() {
       </div>
 
       {/* Investing in communities  */}
-      {/* Charity/pro bono  */}
+      {/* Who We Are  */}
+      <div className="flex flex-col px-3 py-12 sm:px-6 sm:py-20 sm:text-foreground/80 lg:flex-row lg:gap-3 lg:py-24">
+        {/* Title  */}
+        <div className="lg:flex-1 lg:pl-12">
+          <h5 className="mb-4 font-serif text-xl tracking-wide">
+            Investing in Justice and Our Communities
+          </h5>
+          <div className="mb-5 h-px max-w-12 bg-foreground dark:bg-muted-foreground sm:bg-foreground/55" />
+        </div>
+
+        {/* Content  */}
+        <div className="lg:flex-[5] lg:pr-12">
+          <p className="antialiased sm:text-lg">
+            We are committed to advancing the communities in which we live and
+            work, and we dedicate substantial energy, talent and resources to
+            meaningful causes and initiatives that reflect our values and
+            vision. For us, it’s simple. We have a responsibility to make a
+            difference and to help our communities succeed.
+          </p>
+        </div>
+      </div>
+
+      {/* Charity  */}
+      <div className="flex flex-col gap-4 px-3 pb-12 sm:flex-row sm:flex-wrap sm:justify-center sm:gap-12 sm:px-6 sm:pb-20">
+        <div className="group max-w-md cursor-pointer border border-primary px-8 py-24 text-center transition-all duration-500 hover:border-transparent hover:shadow-2xl sm:w-[43%] lg:w-[29%]">
+          <p className="font-serif text-2xl text-primary">
+            Pro Bono
+          </p>
+          <p className="mt-4 text-lg text-muted-foreground antialiased group-hover:text-primary">
+            We are dedicated to providing free legal services to clients who cannot afford counsel
+          </p>
+        </div>
+
+        <div className="group group max-w-md cursor-pointer border border-primary px-8 py-24 text-center transition-all duration-500 hover:border-transparent hover:shadow-2xl sm:w-[43%] lg:w-[29%]">
+          <p className="font-serif text-2xl text-primary">
+            Charitable Giving
+          </p>
+          <p className="mt-4 text-lg text-muted-foreground antialiased group-hover:text-primary">
+            We proudly support nonprofits and law-related organizations with generous financial contributions
+          </p>
+        </div>
+
+        <div className="group max-w-md cursor-pointer border border-primary px-8 py-20 text-center transition-all duration-500 hover:border-transparent hover:shadow-2xl sm:w-[43%] lg:w-[29%]">
+          <p className="font-serif text-2xl text-primary">
+            Sustainability
+          </p>
+          <p className="mt-4 text-lg text-muted-foreground antialiased group-hover:text-primary">
+            We are committed to reducing our environment footprint, creating efficient service for clients and a vibrant work atmosphere for employees
+          </p>
+        </div>
+      </div>
+
       {/* footer  */}
     </div>
   );
