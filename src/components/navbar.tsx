28ca24652f7c89c921f199addd34da2f5e6e3049
mathew bushuru <bushuruwafulamathew@gmail.com>
2025-11-10 02:57:33 -0800

    Add experiment v2 blue theme and make it default, add dropdown to enable fallback to v1

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,16 +1,30 @@
-import { GanttChart } from "lucide-react";
+import { GanttChart, ChevronDown } from "lucide-react";
 
 import ThemeToggle from "@/components/theme-toggle";
 import SideNavigation from "@/components/side-navigation";
+
+import Button from "@/components/primitives/button";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+} from "@/components/primitives/dropdown-menu";
 import {
   Sheet,
   SheetTrigger,
   SheetContent,
 } from "@/components/primitives/sheet";
 
+import useTheme from "@/hooks/use-theme";
+
 function Navbar() {
+  const { version, setVersion } = useTheme();
+
+  const versionLabel = version === "v1" ? "V1 Theme" : "V2 Theme";
+
   return (
-    <div className="fixed left-0 right-0 top-0 z-50 flex h-14 w-full items-center justify-between px-4 py-2 shadow-md backdrop-blur-md dark:border-b">
+    <div className="fixed top-0 right-0 left-0 z-50 flex h-14 w-full items-center justify-between px-4 py-2 shadow-md backdrop-blur-md dark:border-b">
       <div className="flex items-center">
         <Sheet>
           <SheetTrigger>
@@ -26,7 +40,26 @@ function Navbar() {
           @mathewbushuru
         </span>
       </div>
-      <ThemeToggle />
+
+      <div className="flex items-center">
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button size="sm" variant="secondary" className="lg:mr-2">
+              {versionLabel} <ChevronDown className="ml-1 h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuItem onClick={() => setVersion("v1")}>
+              V1 Theme
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setVersion("v2")}>
+              V2 Theme (Experimental)
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+
+        <ThemeToggle />
+      </div>
     </div>
   );
 }
--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,50 +1,62 @@
 import { createContext, useState, useEffect } from "react";
 
-type Theme = "dark" | "light";
+type TTheme = "dark" | "light";
+type TVersion = "v1" | "v2";
 
-type ThemeProviderProps = {
+type TThemeProviderProps = {
   children: React.ReactNode;
-  defaultTheme?: Theme;
+  defaultTheme?: TTheme;
+  defaultVersion?: TVersion;
   storageKey?: string;
 };
 
-type ThemeProviderState = {
-  theme: Theme;
-  setTheme: (theme: Theme) => void;
+type TThemeProviderState = {
+  theme: TTheme;
+  version: TVersion;
+  setTheme: (theme: TTheme) => void;
+  setVersion: (version: TVersion) => void;
 };
 
-const initialState: ThemeProviderState = {
+const initialState: TThemeProviderState = {
   theme: "light",
+  version: "v2",
   setTheme: () => null,
+  setVersion: () => null,
 };
 
 export const ThemeProviderContext =
-  createContext<ThemeProviderState>(initialState);
+  createContext<TThemeProviderState>(initialState);
 
 export default function ThemeProvider({
   children,
   defaultTheme = "light",
+  defaultVersion = "v2",
   storageKey = "mb-design-system-theme",
   ...props
-}: ThemeProviderProps) {
-  const [theme, setTheme] = useState<Theme>(
-    () => (localStorage.getItem(storageKey) as Theme) || defaultTheme,
+}: TThemeProviderProps) {
+  const [theme, setTheme] = useState<TTheme>(
+    () => (localStorage.getItem(storageKey) as TTheme) || defaultTheme,
   );
+
+  const [version, setVersion] = useState(defaultVersion);
 
   useEffect(() => {
     const root = window.document.documentElement;
 
-    root.classList.remove("light", "dark");
+    root.classList.remove("light", "dark", "v1", "v2");
 
     root.classList.add(theme);
-  }, [theme]);
+    root.classList.add(version);
+  }, [theme, version]);
 
   const value = {
     theme,
-    setTheme: (theme: Theme) => {
+    version,
+    setTheme: (theme: TTheme) => {
       localStorage.setItem(storageKey, theme);
       setTheme(theme);
     },
+    setVersion: (version: TVersion) => setVersion(version),
   };
 
   return (
